65cb2aa6daf93528eea0872f51ec9d5dafbf5fe1
Programmer Rakibul <rakibul00206@gmail.com>
2025-12-08 00:44:57 +0600

    dashboard logo added

--- src/layouts/DashboardLayout/DashboardLayout.jsx
+++ src/layouts/DashboardLayout/DashboardLayout.jsx
@@ -1,6 +1,7 @@
 import { Link, Outlet } from "react-router";
 import { MdLibraryAdd } from "react-icons/md";
 import { FaUser } from "react-icons/fa";
+import logo from "../../assets/logo.png";
 
 const DashboardLayout = () => {
   return (
@@ -47,30 +48,17 @@ const DashboardLayout = () => {
             aria-label="close sidebar"
             className="drawer-overlay"
           ></label>
-          <div className="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-14 is-drawer-open:w-64">
+          <div className="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-20 is-drawer-open:w-64">
             {/* Sidebar content here */}
-            <ul className="menu w-full grow">
+            <ul className="menu w-full grow is-drawer-close:items-center">
               <li>
-                <button
+                <Link
+                  to="/"
                   className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
-                  data-tip="Homepage"
                 >
-                  {/* Home icon */}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    strokeLinejoin="round"
-                    strokeLinecap="round"
-                    strokeWidth="2"
-                    fill="none"
-                    stroke="currentColor"
-                    className="my-1.5 inline-block size-4"
-                  >
-                    <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"></path>
-                    <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-                  </svg>
-                  <span className="is-drawer-close:hidden">Homepage</span>
-                </button>
+                  <img src={logo} alt="BookWagon" className="max-w-10" />
+                  <span className="is-drawer-close:hidden text-2xl font-bold">BookWagon</span>
+                </Link>
               </li>
 
               <li>
