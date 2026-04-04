45eeb16fe9a66c03e14b391cc316946fe667ffc0
Md. Rakibul Islam <rakibul00206@gmail.com>
2026-04-04 13:23:54 +0600

    (modify): post user api url

--- src/utilities/postUser.js
+++ src/utilities/postUser.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
 export const postUser = async (user) => {
-  const newUser = {
-    name: user.displayName,
-    email: user.email,
-    photoURL: user.photoURL,
-  };
-
-  const apiBaseUrl = import.meta.env.VITE_SERVER_URL;
-
   try {
-    const { data } = await axios.post(`${apiBaseUrl}/users`, newUser);
+    const newUser = {
+      name: user.displayName,
+      email: user.email,
+      photoURL: user.photoURL,
+    };
+
+    const url = `${import.meta.env.VITE_SERVER_URL}/api/users`;
+
+    const { data } = await axios.post(url, newUser);
     return data;
   } catch (error) {
     console.error("Error posting user:", error);
